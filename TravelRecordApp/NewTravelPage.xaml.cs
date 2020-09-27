4451d2bf83d24ce7ba899669c4f4b14cfd59d890
Bartek Sikorski <bartek.sikorski@gmail.com>
2020-09-27 12:08:08 +0200

    Sqlite - odczyt i preentacja danych

--- TravelRecordApp/NewTravelPage.xaml.cs
+++ TravelRecordApp/NewTravelPage.xaml.cs
@@ -25,21 +25,20 @@ namespace TravelRecordApp
                 Experience = Experience.Text
             };
 
-            SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation);
-            conn.CreateTable<Post>();
-            int rows = conn.Insert(post);
-            conn.Close();
-
-            if (rows > 0)
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
             {
-                DisplayAlert("Success", "Experience added successfuly", "OK");
-            } else
-            {
-                DisplayAlert("Failure", "Experience not added successfuly", "OK");
+                conn.CreateTable<Post>();
+                int rows = conn.Insert(post);
 
+                if (rows > 0)
+                {
+                    DisplayAlert("Success", "Experience added successfuly", "OK");
+                }
+                else
+                {
+                    DisplayAlert("Failure", "Experience not added successfuly", "OK");
+                }
             }
-
-
         }
     }
 }
